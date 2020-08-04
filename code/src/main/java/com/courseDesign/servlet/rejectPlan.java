6335d5f4aa7c56e861663d048761e5f829a824f5
yiyayybj <46925202+yiyayybj@users.noreply.github.com>
2020-08-04 22:26:02 +0800

    Create rejectPlan.java

---
+++ code/src/main/java/com/courseDesign/servlet/rejectPlan.java
@@ -0,0 +1,48 @@
+package com.courseDesign.servlet;
+
+import com.alibaba.fastjson.JSONObject;
+import com.courseDesign.dao.BaseDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+@WebServlet(name = "rejectPlan",urlPatterns = "/rejectPlan.do")
+public class rejectPlan  extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        response.setHeader("content-type","text/html;charset=UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        /* 星号表示所有的异域请求都可以接受， */
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+        PrintWriter out=response.getWriter();
+        // 读取请求内容
+        BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream(),"utf-8"));
+        String line = null;
+        StringBuilder sb = new StringBuilder();
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        HashMap map1 = JSONObject.parseObject(sb.toString(), HashMap.class);
+        int is_approved=Integer.parseInt(map1.get("is_approved").toString());
+        BaseDao baseDao=new BaseDao();
+        String sql="update plan set is_approve=0";
+        baseDao.executeUpdate(sql,is_approved);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
+
+
