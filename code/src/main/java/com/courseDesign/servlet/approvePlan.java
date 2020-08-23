302ffaa1aa8202089abe1b5303c4d753a91409f0
yiyayybj <46925202+yiyayybj@users.noreply.github.com>
2020-08-23 17:18:42 +0800

    Update approvePlan.java

--- code/src/main/java/com/courseDesign/servlet/approvePlan.java
+++ code/src/main/java/com/courseDesign/servlet/approvePlan.java
@@ -1,18 +1,15 @@
 package com.courseDesign.servlet;
 
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONArray;
 import com.courseDesign.dao.BaseDao;
-
+import com.courseDesign.javabean.plan;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.HashMap;
 @WebServlet(name = "approvePlan",urlPatterns = "/approvePlan.do")
 public class approvePlan extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
