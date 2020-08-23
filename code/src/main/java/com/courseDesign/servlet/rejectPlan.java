32ed0fce864eeb755fb26ef4d797ef32f8dd2432
yiyayybj <46925202+yiyayybj@users.noreply.github.com>
2020-08-23 17:19:50 +0800

    Update rejectPlan.java

--- code/src/main/java/com/courseDesign/servlet/rejectPlan.java
+++ code/src/main/java/com/courseDesign/servlet/rejectPlan.java
@@ -1,18 +1,15 @@
 package com.courseDesign.servlet;
 
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONArray;
 import com.courseDesign.dao.BaseDao;
-
+import com.courseDesign.javabean.plan;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.HashMap;
+
 
 @WebServlet(name = "rejectPlan",urlPatterns = "/rejectPlan.do")
 public class rejectPlan  extends HttpServlet {
